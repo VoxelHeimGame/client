bbf6a418be5d80bcbb7f20c2d719df457174d7df
Ivan Cavero <ivangonzalezcavero@gmail.com>
2024-12-03 21:43:56 +0100

    fix: linter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
-/* @refresh reload */
 import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 const root = document.getElementById('root')
 
-render(() => <App />, root!)
+if (root instanceof HTMLElement) {
+  render(() => <App />, root)
+} else {
+  console.error('Root element not found')
+}
+
